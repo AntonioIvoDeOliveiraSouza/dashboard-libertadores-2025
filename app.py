7326a772b1661907d154447556da037d0cef401f
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-11-07 10:39:20 -0300

    feat: club map

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pandas as pd
-from queries import artilheiro
+from queries import artilheiro,time_pais
 from db_connection import get_connection
 import plotly.express as px
+
+st.set_page_config(layout="wide") #because wide is better than stretch
 
 conn = get_connection()
 
 st.title("Estatísticas da Conmebol Libertadores 2025")
 st.divider()
 
-col1,col2 = st.columns(2)
+col1,_,col2 = st.columns([2,0.5,2]) #setting a gap
 
 with col1:
     st.subheader("Artilharia (Eliminatórias)")
@@ -27,3 +29,42 @@ with col1:
     }
     fig = px.bar(df_artilheiro,x="jogador",y="gols_no_mata_mata",color="clube",color_discrete_map=color_artilharia, labels={"jogador": "Artilheiros","gols_no_mata_mata":"Gols"})
     st.plotly_chart(fig)
+
+with col2:
+    color_pais = {
+        "Argentina":"#09b2b8",
+        "Brasil": "#048a0b",
+        "Colômbia": "#4CC952",
+        "Equador": "#18048a",
+        "Paraguai": "#8a0404",
+        "Peru": "#464646",
+        "Uruguai": "#fffb00",
+    }
+    traducao = { #Translation because the data is in portuguese
+        "Brasil": "Brazil",
+        "Argentina": "Argentina",
+        "Uruguai": "Uruguay",
+        "Chile": "Chile",
+        "Paraguai": "Paraguay",
+        "Colômbia": "Colombia",
+        "Equador": "Ecuador",
+        "Peru": "Peru",
+        "Bolívia": "Bolivia",
+        "Venezuela": "Venezuela"
+    }
+    st.header("Times por país")
+    df_pais = time_pais(conn)
+    df_pais["pais_en"] = df_pais["pais"].map(traducao)
+    plot = px.scatter_geo(
+        df_pais,locations="pais_en", 
+        locationmode="country names", 
+        scope="south america",
+        hover_name="pais",
+        color="pais",
+        color_discrete_map=color_pais,
+        labels={"pais_en":"País"}
+    )
+    st.plotly_chart(plot, use_container_width=True)
+
+    st.dataframe(time_pais(conn))
+    
